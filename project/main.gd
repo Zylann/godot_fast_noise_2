6f48e9cde7d6209aff0c7ede9460825fe271d2fc
Marc Gilleron <marc.gilleron@gmail.com>
2024-12-19 01:13:27 +0000

    Basic test

--- project/main.gd
+++ project/main.gd
@@ -1,5 +1,10 @@
 extends Node
 
+@onready var _texture_rect : TextureRect = $TextureRect
+
 
 func _ready():
 	var noise := FastNoise2.new()
+	var image := noise.get_image(512, 512, false, false, false)
+	var texture := ImageTexture.create_from_image(image)
+	_texture_rect.texture = texture
